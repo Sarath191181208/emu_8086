029a270abeff6860086e65eebc506c015d4aca1e
Sarath <vssarathc04@gmail.com>
2023-09-26 21:46:52 +0530

    + compile_and_test_str fn for testing

--- src-tauri/src/cpu/instructions/test_macro.rs
+++ src-tauri/src/cpu/instructions/test_macro.rs
@@ -1,3 +1,5 @@
+use crate::{compiler::compile_lines, cpu::CPU, memory::Memory};
+
 #[macro_export]
 macro_rules! generate_test {
     ($test_name:ident, $instructions:expr, $compare: expr) => {
@@ -36,4 +38,22 @@ macro_rules! generate_test_with_cycles {
             }
         }
     };
+}
+
+pub fn compile_and_test_str(code: &str, cycles: usize, expected_fn: fn(&CPU, &Memory)) {
+    let mut cpu = CPU::new();
+    let mut mem = Memory::new();
+    cpu.reset(&mut mem);
+    
+    let (compiled_bytes, _, is_org_defined) = compile_lines(code, false).unwrap();
+    if is_org_defined {
+        cpu.set_org_defined()
+    }
+    cpu.write_instructions(&mut mem, &compiled_bytes);
+
+    for _ in 0..cycles {
+        cpu.execute(&mut mem);
+    }
+
+    expected_fn(&cpu, &mem);
 }
