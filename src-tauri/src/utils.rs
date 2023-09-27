cb10dd3b4851a1f03d6234318392e225656b111b
Sarath <vssarathc04@gmail.com>
2023-09-27 20:53:53 +0530

    + either ruitl

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -5,10 +5,10 @@ pub enum Either<T, U> {
 
 // have a special implmentation for T = u8 and U = u16 
 impl Either<u8, u16> {
-    pub fn get_as_u16(self) -> u16 {
-        match self {
-            Either::Left(x) => x as u16,
-            Either::Right(x) => x,
+    pub fn get_as_u16(&self) -> u16 {
+        match &self {
+            Either::Left(x) => *x as u16,
+            Either::Right(x) => *x,
         }
     }
 }
