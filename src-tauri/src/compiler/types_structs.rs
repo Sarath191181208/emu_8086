b8a7d31a3094224a8da70ee918dc14104dd50fa5
Sarath <vssarathc04@gmail.com>
2023-09-28 18:54:00 +0530

    struct naming changes label -> Variable

--- src-tauri/src/compiler/types_structs.rs
+++ src-tauri/src/compiler/types_structs.rs
@@ -19,9 +19,10 @@ pub type NumberOfBytes = u16; // i.e size of mem
 pub type IsLabelBeforeRef = bool; // i.e is label before reference
 pub type LabelAddressMap = HashMap<Label, LineNumber>;
 
-pub type VariableReferenceMap = HashMap<Label, (VariableType, ArrayIndex)>;
-pub type VariableAddressMap = HashMap<Label, (VariableType, NumberOfBytes)>;
-pub type VariableAddressDefinitionMap = HashMap<Label, (VariableType, LineNumber)>;
+pub type Variable = Label;
+pub type VariableReferenceMap = HashMap<Variable, (VariableType, ArrayIndex)>;
+pub type VariableAddressMap = HashMap<Variable, (VariableType, NumberOfBytes)>;
+pub type VariableAddressDefinitionMap = HashMap<Variable, (VariableType, LineNumber)>;
 
 // This struct is used to store the compiled bytes of the coverted line
 // for ex: MOV AX, BX
@@ -55,8 +56,8 @@ pub(crate) struct CompiledLine {
     pub compiled_bytes_ref: Vec<CompiledBytesReference>,
     pub labels: Vec<String>,
     pub label_idx_map: std::collections::HashMap<String, (Token, TokenIndex)>,
-    pub label_reference_map: VariableReferenceMap,
-    pub label_abs_address_map: VariableAddressDefinitionMap,
+    pub variable_reference_map: VariableReferenceMap,
+    pub variable_abs_address_map: VariableAddressDefinitionMap,
 }
 
 impl CompiledLine {
@@ -66,8 +67,8 @@ impl CompiledLine {
             compiled_bytes_ref: Vec::new(),
             labels: Vec::new(),
             label_idx_map: std::collections::HashMap::new(),
-            label_reference_map: std::collections::HashMap::new(),
-            label_abs_address_map: std::collections::HashMap::new(),
+            variable_reference_map: std::collections::HashMap::new(),
+            variable_abs_address_map: std::collections::HashMap::new(),
         }
     }
 
@@ -75,8 +76,8 @@ impl CompiledLine {
         self.compiled_bytes.extend(other.compiled_bytes);
         self.compiled_bytes_ref.extend(other.compiled_bytes_ref);
         self.label_idx_map.extend(other.label_idx_map);
-        self.label_reference_map.extend(other.label_reference_map);
-        self.label_abs_address_map
-            .extend(other.label_abs_address_map);
+        self.variable_reference_map.extend(other.variable_reference_map);
+        self.variable_abs_address_map
+            .extend(other.variable_abs_address_map);
     }
 }
