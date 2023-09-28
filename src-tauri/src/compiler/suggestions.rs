47074b0bdd1de4becbea7d36f5ddb0dd15d4be9b
Sarath <vssarathc04@gmail.com>
2023-09-28 21:12:58 +0530

    + define data in suggestions

--- src-tauri/src/compiler/suggestions.rs
+++ src-tauri/src/compiler/suggestions.rs
@@ -1,6 +1,6 @@
 use serde::Serialize;
 
-use super::{tokens::{instructions::Instructions, registers16bit::Registers16bit, registers8bit::Registers8bit}, types_structs::{Label, Variable}};
+use super::types_structs::{Label, Variable};
 
 
 #[derive(Debug)]
@@ -9,6 +9,8 @@ pub enum SuggestionType {
 
     Registers16bit(&'static str),
     Registers8bit(&'static str),
+
+    DefineData(&'static str),
     
     Variables16bit(Variable),
     Variables8bit(Variable),
@@ -23,31 +25,16 @@ impl Serialize for SuggestionType {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer {
-        match self {
-            SuggestionType::Instruction(instruction) => {
-                serializer.serialize_str(&instruction.to_string())
-            }
-            SuggestionType::Registers16bit(register) => {
-                serializer.serialize_str(&register.to_string())
-            }
-            SuggestionType::Registers8bit(register) => {
-                serializer.serialize_str(&register.to_string())
-            }
-            SuggestionType::Variables16bit(variable) => {
-                serializer.serialize_str(&variable.to_string())
-            }
-            SuggestionType::Variables8bit(variable) => {
-                serializer.serialize_str(&variable.to_string())
-            }
-            SuggestionType::Label(label) => {
-                serializer.serialize_str(&label.to_string())
-            }
-            SuggestionType::Constant16bit(constant) => {
-                serializer.serialize_u16(*constant)
-            }
-            SuggestionType::Constant8bit(constant) => {
-                serializer.serialize_u8(*constant)
-            }
+        match &self{
+            SuggestionType::Instruction(x) => serializer.serialize_str(x),
+            SuggestionType::Registers16bit(x) => serializer.serialize_str(x),
+            SuggestionType::Registers8bit(x) => serializer.serialize_str(x),
+            SuggestionType::DefineData(x) => serializer.serialize_str(x),
+            SuggestionType::Variables16bit(x) => serializer.serialize_str(&x.to_string()),
+            SuggestionType::Variables8bit(x) => serializer.serialize_str(&x.to_string()),
+            SuggestionType::Label(x) => serializer.serialize_str(&x.to_string()),
+            SuggestionType::Constant16bit(x) => serializer.serialize_u16(*x),
+            SuggestionType::Constant8bit(x) => serializer.serialize_u8(*x),
         }
     }
 }
