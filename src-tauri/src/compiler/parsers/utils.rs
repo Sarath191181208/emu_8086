47a7d7f50b0f523ff3c37075ae0257d750d0ad3f
Sarath <vssarathc04@gmail.com>
2023-10-02 23:15:03 +0530

    + get_as_0x00_0x3f_pattern

--- src-tauri/src/compiler/parsers/utils.rs
+++ src-tauri/src/compiler/parsers/utils.rs
@@ -8,6 +8,12 @@ use crate::compiler::{
     CompiledBytesReference,
 };
 
+pub fn get_as_0x00_0x3f_pattern(high_reg_idx: u8, low_reg_idx: u8) -> u8{
+    let ins = (0x00) | (high_reg_idx / 2) << 4;
+    let ins2 = low_reg_idx | (high_reg_idx % 2) << 3;
+    ins | ins2
+}
+
 pub fn get_as_0xc0_0xff_pattern(high_reg_idx: u8, low_reg_idx: u8) -> u8 {
     let ins = (0xC0) | (high_reg_idx / 2) << 4;
     let ins2 = low_reg_idx | (high_reg_idx % 2) << 3;
