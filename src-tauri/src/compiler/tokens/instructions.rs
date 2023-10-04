645384cb07491b47a05a20cbc3538061e1d5fae3
Sarath <vssarathc04@gmail.com>
2023-10-04 19:41:14 +0530

    + loop ins

--- src-tauri/src/compiler/tokens/instructions.rs
+++ src-tauri/src/compiler/tokens/instructions.rs
@@ -1,19 +1,14 @@
 use strum_macros::{Display, EnumIter, EnumString, IntoStaticStr};
 
 #[derive(Debug, Clone, IntoStaticStr, Display, PartialEq, Eq, EnumString, EnumIter)]
+#[strum(ascii_case_insensitive)]
 pub(crate) enum Instructions {
-    #[strum(ascii_case_insensitive)]
     Mov,
-    #[strum(ascii_case_insensitive)]
     Add,
-    #[strum(ascii_case_insensitive)]
     Inc,
-    #[strum(ascii_case_insensitive)]
     Dec,
-    #[strum(ascii_case_insensitive)]
     Sub,
-    #[strum(ascii_case_insensitive)]
     Mul,
-    #[strum(ascii_case_insensitive)]
     Jmp,
+    Loop,
 }
