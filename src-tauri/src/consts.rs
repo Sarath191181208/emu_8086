66593b397d38f4243f1b4e1afd77f52fd3d78f48
Sarath <vssarathc04@gmail.com>
2023-10-04 14:46:00 +0530

    internal type of u20 to i32 for ease of use

--- src-tauri/src/consts.rs
+++ src-tauri/src/consts.rs
@@ -1,19 +1,13 @@
 pub type Byte = u8;
 pub type Word = u16;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 #[repr(transparent)]
-pub struct U20(u32);
+pub struct U20(i32);
 
 impl std::fmt::Display for U20 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let is_negative = self.is_negative();
-        let value = self.get_value();
-        if is_negative {
-            write!(f, "-{:#06x}", value)
-        } else {
-            write!(f, "{:#6x}", value)
-        }
+        write!(f, "{:#06x}", self.0)
     }
 }
 
@@ -22,43 +16,36 @@ impl U20 {
     pub const MAX: U20 = U20(1 << 20);
 
     pub fn new(value: u32) -> U20 {
-        assert!(value <= Self::MAX.0);
+        assert!(
+            value <= Self::MAX.0 as u32,
+            "The value provided {:#06x} is greater than the max value {:#06x}",
+            value,
+            Self::MAX.0
+        );
+        U20(value as i32)
+    }
+
+    fn from_i32(value: i32) -> U20{
+        assert!(
+            value <= Self::MAX.0,
+            "The value provided {:#06x} is greater than the max value {:#06x}",
+            value,
+            Self::MAX.0
+        );
         U20(value)
     }
 
     pub fn get(&self) -> u32 {
-        self.0
+        self.0 as u32
     }
 
     pub fn as_segment_offset(&self) -> (u16, u16) {
-        let value = self.0;
+        let value = self.0 as u32;
         // i.e value = 0x000F_FFFF
         // segment = 0x000F, offset = 0xFFFF
         let segment = (value >> 16) as u16;
         let offset = (value & 0xFFFF) as u16;
         (segment, offset)
-    }
-
-    fn is_negative(&self) -> bool {
-        // check if the num is u8
-        if self.0 <= 0xFF && self.0 > 0x80 {
-            return true;
-        }
-        if self.0 <= 0xFFFF && self.0 > 0x8000 {
-            return true;
-        }
-        false
-    }
-
-    fn get_value(&self) -> u32 {
-        if self.is_negative() {
-            match self.0 {
-                0x80..=0xFF => return 0xFF - self.0 + 1,
-                0x8000..=0xFFFF => return 0xFFFF - self.0 + 1,
-                _ => return self.0,
-            }
-        }
-        self.0
     }
 }
 
@@ -70,13 +57,15 @@ impl From<u32> for U20 {
 
 impl From<u16> for U20 {
     fn from(value: u16) -> Self {
-        U20::new(value as u32)
+        let i_16 = value as i16;
+        U20::from_i32(i_16 as i32)
     }
 }
 
 impl From<u8> for U20 {
     fn from(value: u8) -> Self {
-        U20::new(value as u32)
+        let i_8 = value as i8;
+        U20::from_i32(i_8 as i32)
     }
 }
 
@@ -90,16 +79,7 @@ impl std::ops::Add for U20 {
     type Output = U20;
 
     fn add(self, other: U20) -> U20 {
-        match (self.is_negative(), other.is_negative()) {
-            (true, true) | (false, false) => {
-                let (res, _) = self.get_value().overflowing_add(other.get_value());
-                U20::new(res)
-            }
-            (true, false) | (false, true) => {
-                let (res, _) = self.get_value().overflowing_sub(other.get_value());
-                U20::new(res)
-            }
-        }
+        U20::from_i32(self.0 + other.0)
     }
 }
 
@@ -107,7 +87,7 @@ impl std::ops::Sub for U20 {
     type Output = U20;
 
     fn sub(self, other: U20) -> U20 {
-        U20::new(self.0 - other.0)
+        U20::from_i32(self.0 - other.0)
     }
 }
 
@@ -115,7 +95,7 @@ impl std::ops::Mul for U20 {
     type Output = U20;
 
     fn mul(self, other: U20) -> U20 {
-        U20::new(self.0 * other.0)
+        U20::from_i32(self.0 * other.0)
     }
 }
 
@@ -123,7 +103,7 @@ impl std::ops::Div for U20 {
     type Output = U20;
 
     fn div(self, other: U20) -> U20 {
-        U20::new(self.0 / other.0)
+        U20::from_i32(self.0 / other.0)
     }
 }
 
@@ -131,7 +111,7 @@ impl std::ops::Rem for U20 {
     type Output = U20;
 
     fn rem(self, other: U20) -> U20 {
-        U20::new(self.0 % other.0)
+        U20::from_i32(self.0 % other.0)
     }
 }
 
@@ -139,7 +119,7 @@ impl std::ops::BitAnd for U20 {
     type Output = U20;
 
     fn bitand(self, other: U20) -> U20 {
-        U20::new(self.0 & other.0)
+        U20::from_i32(self.0 & other.0)
     }
 }
 
@@ -147,7 +127,7 @@ impl std::ops::BitOr for U20 {
     type Output = U20;
 
     fn bitor(self, other: U20) -> U20 {
-        U20::new(self.0 | other.0)
+        U20::from_i32(self.0 | other.0)
     }
 }
 
@@ -155,7 +135,7 @@ impl std::ops::BitXor for U20 {
     type Output = U20;
 
     fn bitxor(self, other: U20) -> U20 {
-        U20::new(self.0 ^ other.0)
+        U20::from_i32(self.0 ^ other.0)
     }
 }
 
@@ -163,6 +143,6 @@ impl std::ops::Not for U20 {
     type Output = U20;
 
     fn not(self) -> U20 {
-        U20::new(!self.0)
+        U20::from_i32(!self.0)
     }
 }
