4a13c57b0578a70e00ead885243c26e349824117
Sarath <vssarathc04@gmail.com>
2023-10-06 01:44:39 +0530

    utils for either<i8, i16>

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -24,6 +24,41 @@ impl Either<u8, u16> {
     }
 }
 
+impl Either<i8, i16>{
+    pub fn get_as_unsigned(&self) -> Either<u8, u16> {
+        match &self {
+            Either::Left(x) => Either::Left(*x as u8),
+            Either::Right(x) => Either::Right(*x as u16),
+        }
+    }
+}
+
+impl From<i8> for Either<i8, i16> {
+    fn from(x: i8) -> Self {
+        Self::Left(x)
+    }
+}
+
+impl From<i16> for Either<i8, i16> {
+    fn from(x: i16) -> Self {
+        Self::Right(x)
+    }
+}
+
+// also impl from usize check the least possilbe and assign i8 or i16
+// also check the min limit 
+impl From<usize> for Either<i8, i16> {
+    fn from(x: usize) -> Self {
+        let is_x_in_i8_range = x <= i8::MAX as usize && x >= i8::MIN as usize;
+        if is_x_in_i8_range{
+            Self::Left(x as i8)
+        } else {
+            Self::Right(x as i16)
+        }
+    }
+}
+
+
 #[derive(Debug, Serialize)]
 pub struct TokenPosition {
     line_number: usize,
