c2aa32eb8e761aeb49a9724d595754fe2350503f
Sarath <vssarathc04@gmail.com>
2023-10-19 20:24:54 +0530

    🐞FIX: the ui wrongly updating for 8bit reg

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,14 @@
 const padHex = (s: string) => s.padStart(2, "0");
 
 export const getHighHex = (v: number) => {
-  const hex = v.toString(16).toUpperCase();
+  const highBits = 0xFF00 & v;
+  const hex = highBits.toString(16).toUpperCase();
   return "0x" + padHex(hex.slice(0, 2));
 };
 
 export const getLowHex = (v: number) => {
-  const hex = v.toString(16).toUpperCase();
-  return "0x" + padHex(hex.slice(2, 4));
+  const lowBits = 0x00FF & v;
+  const hex = lowBits.toString(16).toUpperCase();
+  return "0x" + padHex(hex);
 };
 
