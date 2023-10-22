1100380f636e5155821510a29dd39a9142b1f662
Sarath <vssarathc04@gmail.com>
2023-10-22 17:45:54 +0530

    +util method for tests

--- src-tauri/src/compiler/tests.rs
+++ src-tauri/src/compiler/tests.rs
@@ -22,6 +22,20 @@ macro_rules! test_compile {
     };
 }
 
+
+#[macro_export]
+macro_rules! compile_and_compare_ins{
+    ($test_name: ident, $code: expr, $expected_ins: expr) => {
+        test_compile!(
+            $test_name,
+            $code,
+            |compiled_instructions: &Vec<u8>| {
+                assert_eq!(compiled_instructions, &$expected_ins);
+            }
+        );
+    }
+}
+
 #[allow(dead_code)]
 pub(super) fn generate_num_ins(size: u16) -> String {
     let mut ins = String::new();
