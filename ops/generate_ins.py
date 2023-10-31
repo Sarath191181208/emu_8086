32d95ab0e0cb51ca07596ff4820cb0d6232abefb
Sarath <vssarathc04@gmail.com>
2023-11-01 01:43:42 +0530

    generate test strs

--- ops/generate_ins.py
+++ ops/generate_ins.py
@@ -30,9 +30,26 @@ def seperate(ins: list[str]) -> list[str]:
     return sperated
 
 
-pyperclip.copy( to_str_with_newline( 
-    seperate(
-    to_var_in_each_line( 
-        append_ins(
-            generate_ins(), 0x03) ) )))
+# pyperclip.copy( to_str_with_newline( 
+#     seperate(
+#     to_var_in_each_line( 
+#         append_ins(
+#             generate_ins(), 0x03) ) )))
 
+import random 
+
+reg_16 = ["ax", "cx", "dx", "bx", "sp", "bp", "si", "di"]
+indexed_reg_16 = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
+
+ins_name = "ADD"
+ins: list[str] = []
+for _ in range(0x10):
+    reg_choice = random.choice(reg_16)
+    indexed_reg_choice = random.choice(indexed_reg_16)
+    offset = random.choice((None, random.randint(0x00, 0xFF), random.randint(0x100, 0xFFFF)))
+    if offset is None:
+        ins.append(f"{ins_name} {indexed_reg_choice}, {reg_choice}")
+    else:
+        ins.append(f"{ins_name} [{indexed_reg_choice}+{hex(offset)}], {reg_choice}")
+
+pyperclip.copy("\n".join(ins))
