0087689ba78b53c8d753b35194bc0329819db072
Sarath <vssarathc04@gmail.com>
2023-11-01 08:49:07 +0530

    - deprecated old test fns

--- src-tauri/src/cpu/instructions/sub/register_addressing.rs
+++ src-tauri/src/cpu/instructions/sub/register_addressing.rs
@@ -21,78 +21,76 @@ impl CPU {
 
 #[cfg(test)]
 mod sub_16bit_register_addressing {
-    use crate::{cpu::CPU, generate_test, memory::Memory};
+    use crate::{
+        cpu::{instructions::test_macro::run_code, CPU},
+        generate_test,
+        memory::Memory,
+    };
 
     // sub ax, cx
-    generate_test!(
-        test_sub_ax_cx,
-        (|cpu: &mut CPU, mem: &mut Memory| {
-            cpu.ax = 0x0f0f;
-            cpu.cx = 0x0013;
-            cpu.write_instructions(mem, &[0x2B, 0xC1]);
-        }),
-        (|cpu: &CPU, _: &Memory| {
-            assert_eq!(cpu.ax, 0x0EFC);
-            assert_eq!(cpu.get_flags_as_binary(), 0b00010000)
-        })
-    );
-    // sub bx, dx overflow
-    generate_test!(
-        test_sub_bx_dx_overflow,
-        (|cpu: &mut CPU, mem: &mut Memory| {
-            cpu.bx = 0x0000;
-            cpu.dx = 0x0013;
-            cpu.write_instructions(mem, &[0x2B, 0xDA]);
-        }),
-        (|cpu: &CPU, _: &Memory| {
-            assert_eq!(cpu.bx, 0xFFED);
-            assert_eq!(cpu.get_flags_as_binary(), 0b000110101)
-        })
-    );
-    // sub sp, bp
-    generate_test!(
-        test_sub_sp_bp,
-        (|cpu: &mut CPU, mem: &mut Memory| {
-            cpu.stack_pointer = 0xF000;
-            cpu.base_pointer = 0x0013;
-            cpu.write_instructions(mem, &[0x2B, 0xE5]);
-        }),
-        (|cpu: &CPU, _: &Memory| {
-            assert_eq!(cpu.stack_pointer, 0xEFED);
-            assert_eq!(cpu.get_flags_as_binary(), 0b00110100)
-        })
-    );
+    #[test]
+    fn test_sub_ax_cx() {
+        let code = "
+            MOV AX, 0x0F0F
+            MOV CX, 0x0013
+            SUB AX, CX
+            ";
+        let (cpu, _) = run_code(code, 3);
+        assert_eq!(cpu.ax, 0x0EFC);
+        assert_eq!(cpu.get_flags_as_binary(), 0b00010000)
+    }
+
+    #[test]
+    fn test_sub_bx_dx_overflow(){
+        let code = "
+        MOV BX, 0x00
+        mov DX, 0x13 
+        SUB BX, DX 
+        ";
+        let (cpu, _) = run_code(code, 3);
+        assert_eq!(cpu.bx, 0xFFED);
+        assert_eq!(cpu.get_flags_as_binary(), 0b000110101)
+    }
+
+    #[test]
+    fn test_sub_sp_bp(){
+        let code = "
+        MOV SP, 0xF000
+        mov BP, 0x0013
+        SUB SP, BP
+        ";
+        let (cpu, _) = run_code(code, 3);
+        assert_eq!(cpu.stack_pointer, 0xEFED);
+        assert_eq!(cpu.get_flags_as_binary(), 0b00110100)
+    }
 }
 
 #[cfg(test)]
 mod sub_8bit_register_addressing {
-    use crate::{cpu::CPU, generate_test, memory::Memory};
+    use crate::{cpu::{CPU, instructions::test_macro::run_code}, generate_test, memory::Memory};
 
-    // sub al, cl
-    generate_test!(
-        test_sub_al_cl,
-        (|cpu: &mut CPU, mem: &mut Memory| {
-            cpu.set_ax_low(0x0f);
-            cpu.set_cx_low(0x13);
-            cpu.write_instructions(mem, &[0x2A, 0xC1]);
-        }),
-        (|cpu: &CPU, _: &Memory| {
-            assert_eq!(cpu.get_ax_low(), 0xFC);
-            assert_eq!(cpu.get_flags_as_binary(), 0b00010101)
-        })
-    );
 
-    // sub bl, dh overflow
-    generate_test!(
-        test_sub_bl_dl_overflow,
-        (|cpu: &mut CPU, mem: &mut Memory| {
-            cpu.set_bx_low(0xf0);
-            cpu.set_dx_high(0x13);
-            cpu.write_instructions(mem, &[0x2A, 0xDE]);
-        }),
-        (|cpu: &CPU, _: &Memory| {
-            assert_eq!(cpu.get_bx_low(), 0xDD);
-            assert_eq!(cpu.get_flags_as_binary(), 0b00110100)
-        })
-    );
+    #[test]
+    fn test_sub_al_cl(){
+        let code = "
+        MOV AL, 0x0F
+        MOV CL, 0x13
+        SUB AL, CL
+        ";
+        let (cpu, _) = run_code(code, 3);
+        assert_eq!(cpu.ax, 0xFC);
+        assert_eq!(cpu.get_flags_as_binary(), 0b00010101)
+    }
+
+    #[test]
+    fn test_sub_bl_dl_overflow(){
+        let code = "
+        MOV BL, 0xF0
+        MOV DL, 0x13
+        SUB BL, DL
+        ";
+        let (cpu, _) = run_code(code, 3);
+        assert_eq!(cpu.get_bx_low(), 0xDD);
+        assert_eq!(cpu.get_flags_as_binary(), 0b00110100)
+    }
 }
