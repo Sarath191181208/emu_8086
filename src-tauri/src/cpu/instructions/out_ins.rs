54906dc39a589784c012ebfcaa19daafa17a453a
Sarath <vssarathc04@gmail.com>
2023-11-01 08:03:32 +0530

    removed deprecated testing fns

--- src-tauri/src/cpu/instructions/out_ins.rs
+++ src-tauri/src/cpu/instructions/out_ins.rs
@@ -29,62 +29,48 @@ impl CPU {
 #[cfg(test)]
 mod tests {
     use crate::{
-        cpu::{instructions::test_macro::compile_and_test_str, CPU},
+        cpu::{
+            instructions::test_macro::{compile_and_test_str, run_code},
+            CPU,
+        },
         memory::Memory,
     };
 
     #[test]
     fn out_al_10() {
-        compile_and_test_str(
-            "
+        let code = "
             mov al, 0x10
-            OUT 0x80, AL",
-            2,
-            |cpu: &CPU, _: &Memory| {
-                assert_eq!(cpu.get_port(0x80), 0x10);
-            },
-        );
+            OUT 0x80, AL";
+        let (cpu, _) = run_code(code, 2);
+        assert_eq!(cpu.get_port(0x80), 0x10);
     }
 
     #[test]
     fn out_ax_20() {
-        compile_and_test_str(
-            "
+        let code = "
             mov ax, 0x1020
-            OUT 0x80, AX",
-            2,
-            |cpu: &CPU, _: &Memory| {
-                assert_eq!(cpu.get_port_word(0x80), 0x1020);
-            },
-        );
+            OUT 0x80, AX";
+        let (cpu, _) = run_code(code, 2);
+        assert_eq!(cpu.get_port_word(0x80), 0x1020);
     }
 
     #[test]
     fn out_al_dx() {
-        compile_and_test_str(
-            "
+        let code = "
             mov al, 0x10
             mov dx, 0x80
-            OUT DX, AL",
-            3,
-            |cpu: &CPU, _: &Memory| {
-                cpu.ports.print_non_empty_prots();
-                assert_eq!(cpu.get_port(0x80), 0x10);
-            },
-        );
+            OUT DX, AL";
+        let (cpu, _) = run_code(code, 3);
+        assert_eq!(cpu.get_port(0x80), 0x10);
     }
 
     #[test]
     fn out_ax_dx() {
-        compile_and_test_str(
-            "
+        let code = "
             mov ax, 0x1020
             mov dx, 0x80
-            OUT DX, AX",
-            3,
-            |cpu: &CPU, _: &Memory| {
-                assert_eq!(cpu.get_port_word(0x80), 0x1020);
-            },
-        );
+            OUT DX, AX";
+        let (cpu, _) = run_code(code, 3);
+        assert_eq!(cpu.get_port_word(0x80), 0x1020);
     }
 }
