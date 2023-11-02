b6eccf7e6ca891bb417b68dc542536a5339347e7
Sarath <vssarathc04@gmail.com>
2023-11-03 02:31:52 +0530

    impl xchg

---
+++ src-tauri/src/compiler/parsers/xchg.rs
@@ -0,0 +1,218 @@
+use crate::{
+    compiler::{
+        compilation_error::CompilationError,
+        parsers::utils::{get_as_0xc0_0xff_pattern, push_instruction},
+        tokenized_line::TokenizedLine,
+        CompiledBytesReference,
+    },
+    convert_and_push_instructions,
+    cpu::instructions::add,
+};
+
+use super::{
+    pattern_extractors::{
+        compile_first_ins_reg_pattern::{
+            parse_16bitreg_first_addr_mode, parse_8bitreg_first_addr_mode,
+        },
+        compile_tow_args_whole_ins::{compile_two_args_whole_ins, CompilingBytesForInstruction},
+        compile_two_arguments_patterns::parse_indexed_addr_and_reg,
+        AddressingMode,
+    },
+    utils::{get_8bit_register, get_idx_from_token},
+};
+
+pub(in crate::compiler) fn parse_xchg(
+    tokenized_line: &TokenizedLine,
+    i: usize,
+    compiled_bytes: &mut Vec<u8>,
+    compiled_bytes_ref: &mut Vec<CompiledBytesReference>,
+    addressing_mode: AddressingMode,
+) -> Result<usize, CompilationError> {
+    let token = tokenized_line.get(
+        i,
+        "This shouldn't happen, Please report this bug to the developer".to_string(),
+        None,
+    )?;
+
+    let reg_8bit_and_anything_ins = 0x86;
+    let reg_16bit_and_anything_ins = 0x87;
+    let indexed_addressing_and_anyting_ins = 0x86;
+
+    match addressing_mode {
+        AddressingMode::Registers16bitNumber {
+            high_token: _,
+            low_token,
+            num: _,
+        }
+        | AddressingMode::Register8bitNumber {
+            high_token: _,
+            low_token,
+            num: _,
+        }
+        | AddressingMode::AddressAnd16bitNumber {
+            high_token: _,
+            low_token,
+            address_bytes: _,
+            num: _,
+        }
+        | AddressingMode::AddressAnd8bitNumber {
+            high_token: _,
+            low_token,
+            address_bytes: _,
+            num: _,
+        }
+        | AddressingMode::ByteAddressAnd8bitNumber {
+            high_token: _,
+            low_token,
+            address_bytes: _,
+            num: _,
+        } => {
+            return Err(CompilationError::error_with_token(
+                &low_token,
+                "Invalid addressing mode for XCHG instruction",
+            ));
+        }
+
+        AddressingMode::Registers16bit {
+            high_token,
+            low_token,
+        } => {
+            let high_reg_idx = get_idx_from_token(&high_token)?;
+            let low_reg_idx = get_idx_from_token(&low_token)?;
+            let is_ax_in_low_or_high = high_reg_idx == 0 || low_reg_idx == 0;
+            if is_ax_in_low_or_high {
+                let ins = 0x90 + high_reg_idx + low_reg_idx;
+                convert_and_push_instructions!(
+                    compiled_bytes,
+                    compiled_bytes_ref,
+                    (
+                        &low_token => vec![ins]
+                    )
+                );
+            } else {
+                let ins = get_as_0xc0_0xff_pattern(high_reg_idx, low_reg_idx);
+                convert_and_push_instructions!(
+                    compiled_bytes,
+                    compiled_bytes_ref,
+                    (
+                        token => vec![0x87],
+                        &low_token => vec![ins]
+                    )
+                );
+            }
+            Ok(i + 3)
+        }
+
+        AddressingMode::Register16bitAndAddress {
+            high_token: _,
+            low_token: _,
+            address_bytes: _,
+            register_type: _,
+        }
+        | AddressingMode::Register16bitAndIndexedAddress {
+            high_token: _,
+            low_token: _,
+        }
+        | AddressingMode::Register16bitAndIndexedAddressWithOffset {
+            high_token: _,
+            low_token: _,
+            offset: _,
+        } => parse_16bitreg_first_addr_mode(
+            i,
+            addressing_mode,
+            reg_16bit_and_anything_ins,
+            tokenized_line,
+            token,
+            compiled_bytes,
+            compiled_bytes_ref,
+        ),
+
+        AddressingMode::AddressAnd16bitRegister {
+            high_token,
+            low_token,
+            address_bytes,
+            register_type,
+        } => {
+            // 0x86 0x00..0x07
+            let reg_idx = register_type.get_index_or_err(&low_token)?;
+            convert_and_push_instructions!(
+                compiled_bytes,
+                compiled_bytes_ref,
+                (
+                    token => vec![0x87],
+                    &low_token => vec![0x06 | reg_idx << 3],
+                    &high_token => address_bytes.to_vec()
+                )
+            );
+            Ok(tokenized_line.len())
+        }
+        AddressingMode::AddressAnd8bitRegister {
+            high_token,
+            low_token,
+            address_bytes,
+            register_type,
+        } => {
+            // 0x86 0x00..0x07
+            let reg_idx = register_type.get_as_idx();
+            convert_and_push_instructions!(
+                compiled_bytes,
+                compiled_bytes_ref,
+                (
+                    token => vec![0x86],
+                    &low_token => vec![0x06 | reg_idx << 3],
+                    &high_token => address_bytes.to_vec()
+                )
+            );
+            Ok(tokenized_line.len())
+        }
+
+        AddressingMode::IndexedAddressingAndRegister {
+            high_token,
+            low_token,
+            register_type,
+            addr_type,
+        } => {
+            parse_indexed_addr_and_reg(
+                indexed_addressing_and_anyting_ins,
+                token,
+                &high_token,
+                &low_token,
+                register_type,
+                addr_type,
+                compiled_bytes,
+                compiled_bytes_ref,
+            )?;
+            Ok(tokenized_line.len())
+        }
+
+        AddressingMode::Registers8bit {
+            high_token: _,
+            low_token: _,
+        }
+        | AddressingMode::Register8bitAndIndexedAddress {
+            high_token: _,
+            low_token: _,
+            register_type: _,
+        }
+        | AddressingMode::Register8bitAndIndexedAddressWithOffset {
+            high_token: _,
+            low_token: _,
+            register_type: _,
+            offset: _,
+        }
+        | AddressingMode::Register8bitAndAddress {
+            high_token: _,
+            low_token: _,
+            address_bytes: _,
+            register_type: _,
+        } => parse_8bitreg_first_addr_mode(
+            i,
+            addressing_mode,
+            reg_8bit_and_anything_ins,
+            tokenized_line,
+            token,
+            compiled_bytes,
+            compiled_bytes_ref,
+        ),
+    }
+}
