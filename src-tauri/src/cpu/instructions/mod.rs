5ed1fbb469376ba16678eed293969838b5f8037f
Sarath <vssarathc04@gmail.com>
2023-11-02 14:06:16 +0530

    + xor mod in scope

--- src-tauri/src/cpu/instructions/mod.rs
+++ src-tauri/src/cpu/instructions/mod.rs
@@ -6,21 +6,20 @@ pub mod mov;
 pub mod mul;
 pub mod sub;
 
+pub mod and;
 pub mod call;
 pub mod in_compilation;
 pub mod int;
 pub mod iret;
+pub mod lea;
+pub mod les;
 pub mod loop_ins;
+pub mod or;
 pub mod out_ins;
 pub mod pop;
 pub mod push;
-
-pub mod and;
 pub mod ret;
 pub mod test_ins;
 pub mod test_macro;
-
-pub mod lea;
-pub mod les;
-pub mod or;
 pub(in crate::cpu::instructions) mod utils;
+pub mod xor;
