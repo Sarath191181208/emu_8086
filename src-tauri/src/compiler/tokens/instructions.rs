984f6d9a25e3e1a60b3ff0a80f07a481daafff78
Sarath <vssarathc04@gmail.com>
2023-11-04 15:47:08 +0530

    + adc ins

--- src-tauri/src/compiler/tokens/instructions.rs
+++ src-tauri/src/compiler/tokens/instructions.rs
@@ -3,6 +3,7 @@ use strum_macros::{Display, EnumIter, EnumString, IntoStaticStr};
 #[derive(Debug, Clone, IntoStaticStr, Display, PartialEq, Eq, EnumString, EnumIter, Hash)]
 #[strum(ascii_case_insensitive)]
 pub(crate) enum Instructions {
+    Adc,
     Add,
     And,
     Call,
