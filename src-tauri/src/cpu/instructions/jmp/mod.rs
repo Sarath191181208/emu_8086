bb9ab1fa90396bb2bf26ad861aa688d57d52455f
Sarath <vssarathc04@gmail.com>
2023-11-08 20:21:21 +0530

    test(JMP): deprecated tests -> functioning

--- src-tauri/src/cpu/instructions/jmp/mod.rs
+++ src-tauri/src/cpu/instructions/jmp/mod.rs
@@ -26,61 +26,38 @@ impl CPU {
 }
 #[cfg(test)]
 mod test_8bit_jmp {
-    use crate::{compiler::compile_lines, cpu::CPU, generate_test_with_cycles, memory::Memory};
+    use crate::cpu::instructions::test_macro::run_code;
 
-    generate_test_with_cycles!(
-        jmp_8bit_positive,
-        (|cpu: &mut CPU, mem: &mut Memory| {
-            let (compiled_bytes, _, _) = compile_lines(
-                "
+    #[test]
+    fn jmp_8bit_positive() {
+        let code = "
             label:
                 INC AX
             jmp label
-            ",
-                false,
-            )
-            .unwrap();
-            cpu.write_instructions(mem, &compiled_bytes);
-        }),
-        (|cpu: &CPU, _: &Memory| {
-            assert_eq!(cpu.instruction_pointer, 0x0001);
-            assert_eq!(cpu.ax, 0x0003);
-        }),
-        5
-    );
+            ";
+        let (cpu, _) = run_code(code, 5);
+        assert_eq!(cpu.instruction_pointer, 0x0001);
+        assert_eq!(cpu.ax, 0x0003);
+    }
 
-    generate_test_with_cycles!(
-        jmp_8bit_negative,
-        (|cpu: &mut CPU, mem: &mut Memory| {
-            let (compiled_bytes, _, _) = compile_lines(
-                "
-                INC AX
-                JMP label
-                DEC AX
-                label:
-                INC AX  
-            ",
-                false,
-            )
-            .unwrap();
-            cpu.write_instructions(mem, &compiled_bytes);
-        }),
-        (|cpu: &CPU, _: &Memory| {
-            assert_eq!(cpu.instruction_pointer, 0x0005);
-            assert_eq!(cpu.ax, 0x0002);
-        }),
-        3
-    );
+    #[test]
+    fn jmp_8bit_negative() {
+        let code = "
+        INC AX
+        JMP label
+        DEC AX
+        label:
+        INC AX  
+    ";
+        let (cpu, _) = run_code(code, 3);
+        assert_eq!(cpu.instruction_pointer, 0x0005);
+        assert_eq!(cpu.ax, 0x0002);
+    }
 }
 
 #[cfg(test)]
 mod test_16_bit_jmp {
-    use crate::{
-        compiler::compile_lines,
-        cpu::{instructions::test_macro::compile_and_test_str, CPU},
-        generate_test_with_cycles,
-        memory::Memory,
-    };
+    use crate::cpu::instructions::test_macro::run_code;
 
     fn generate_0x80_long_ins() -> String {
         let mut ins = String::new();
@@ -90,72 +67,48 @@ mod test_16_bit_jmp {
         ins
     }
 
-    generate_test_with_cycles!(
-        jmp_16bit_positive,
-        (|cpu: &mut CPU, mem: &mut Memory| {
-            let (compiled_bytes, _, _) = compile_lines(
-                format!(
-                    "
-            label:
-                {}
-            jmp label
-            ",
-                    generate_0x80_long_ins()
-                )
-                .as_str(),
-                false,
-            )
-            .unwrap();
-            cpu.write_instructions(mem, &compiled_bytes);
-        }),
-        (|cpu: &CPU, _: &Memory| {
-            assert_eq!(cpu.instruction_pointer, 0x0001);
-            assert_eq!(cpu.ax, 0x0081);
-        }),
-        0x82
-    );
+    #[test]
+    fn jmp_16bit_positive() {
+        let code = format!(
+            "
+        label:
+            {}
+        jmp label
+        ",
+            generate_0x80_long_ins()
+        );
+        let (cpu, _) = run_code(&code, 0x82);
+        assert_eq!(cpu.instruction_pointer, 0x0001);
+        assert_eq!(cpu.ax, 0x0081);
+    }
 
-    generate_test_with_cycles!(
-        jmp_16bit_negative,
-        (|cpu: &mut CPU, mem: &mut Memory| {
-            let (compiled_bytes, _, _) = compile_lines(
-                format!(
-                    "
-                INC AX
-                JMP label
-                {}
-                label:
-                INC AX
-            ",
-                    generate_0x80_long_ins()
-                )
-                .as_str(),
-                false,
-            )
-            .unwrap();
-            cpu.write_instructions(mem, &compiled_bytes);
-        }),
-        (|cpu: &CPU, _: &Memory| {
-            assert_eq!(cpu.instruction_pointer, 0x085);
-            assert_eq!(cpu.ax, 0x0002);
-        }),
-        0x3
-    );
+    #[test]
+    fn jmp_16bit_negative() {
+        let code = format!(
+            "
+        INC AX
+        JMP label
+        {}
+        label:
+        INC AX
+        ",
+            generate_0x80_long_ins()
+        );
+        let (cpu, _) = run_code(&code, 0x3);
+        assert_eq!(cpu.instruction_pointer, 0x085);
+        assert_eq!(cpu.ax, 0x0002);
+    }
 
     #[test]
     fn test_jmp_var() {
-        compile_and_test_str(
-            "
-            org 0x100
-            .data
-            var dw 0x0110
-            code: 
-            jmp var
-            ",
-            2,
-            |cpu: &CPU, _: &Memory| {
-                assert_eq!(cpu.instruction_pointer, 0x0102);
-            },
-        );
+        let code = "
+        org 0x100
+        .data
+        var dw 0x0110
+        code: 
+        jmp var
+    ";
+        let (cpu, _) = run_code(code, 2);
+        assert_eq!(cpu.instruction_pointer, 0x0102);
     }
 }
