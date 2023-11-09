3a4a8b9beafae0677643380def8d86c6d08a94df
Sarath <vssarathc04@gmail.com>
2023-11-09 15:59:32 +0530

    Add jae parser test

---
+++ src-tauri/src/tests/compiler/parsers/jae.rs
@@ -0,0 +1,62 @@
+
+use crate::{compile_and_compare_ins, compiler::compile_str, test_compile};
+use pretty_assertions::assert_eq;
+
+
+compile_and_compare_ins!(
+    test_jae,
+    "
+        org 100h
+
+        start:
+        jae label 
+
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100  
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100           
+        mov [0x100], 0x100 
+        mov [0x100], 0x100 
+        mov [0x100], 0x100  
+
+        jae label
+
+        label:  
+
+        jae label
+         
+        jae 0x10   
+        jae start
+        jae 0x00 
+        ",
+    get_compiled_bytes()
+);
+
+fn get_compiled_bytes() -> Vec<u8> {
+    let starting_bytes = [0x72, 0x03, 0xE9, 0x80, 0x00];
+
+    let mid_bytes_repeat = vec![0xC7, 0x06, 0x00, 0x01, 0x00, 0x01].repeat(21);
+
+    let after_bytes = [
+        0x73, 0x00, 0x73, 0xFE, 0x73, 0x0E, 0x72, 0x03, 0xE9, 0x72, 0xFF, 0x73, 0xFE,
+    ];
+
+    let mut compiled_bytes = starting_bytes.to_vec();
+    compiled_bytes.extend(mid_bytes_repeat);
+    compiled_bytes.extend(after_bytes);
+    compiled_bytes
+}
